e632efaf37d47738b40c7575976ecec48147a548
Владислав Кондратенко <Vladkondratencko@gmail.com>
2024-01-04 13:13:17 +0300

    Добавьте файлы проекта.

---
+++ monalisa/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace monalisa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 602);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(392, 576);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -402653185,
+            -1613725636,
+            54210108,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(202, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(480, 480);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 549);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(170, 633);
+            this.panel2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(451, 602);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 633);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(900, 672);
+            this.Name = "Form1";
+            this.Text = "Picture Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ monalisa/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace monalisa
+{
+    public partial class Form1 : Form
+    {
+        bool working = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int xpos = 0;
+            int ypos = 0;
+            //Random random = new Random();
+            for (int i = 0; i != 12; i++)
+            {
+                for (int j = 0; j != 12; j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.Name = i.ToString();
+                    pb.Size = new Size(40, 40);
+                    pb.Location = new Point(xpos, ypos);
+                    //Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                    pb.BackColor = Color.Black;
+                    panel1.Controls.Add(pb);
+                    xpos += 40;
+                }
+                xpos= 0;
+                ypos += 40;
+            }
+        }
+
+        private int currentX = 10;
+        private int currentY = 10;
+        private const int padding = 10;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            working = true;
+            Random random = new Random();
+            uint attempts = (uint)numericUpDown1.Value;
+            for (uint i = 0; i != attempts; i++)
+            {
+                Bitmap miniImage = new Bitmap(120, 120);
+                Bitmap maxImage = new Bitmap(480, 480);
+                foreach (PictureBox pb in panel1.Controls)
+                {
+                    Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                    pb.BackColor = randomColor;
+                }
+
+                using (Graphics g = Graphics.FromImage(miniImage))
+                {
+                    int x = 0;
+                    int y = 0;
+
+                    foreach (PictureBox pb in panel1.Controls)
+                    {
+                        Color color = pb.BackColor;
+
+                        g.FillRectangle(new SolidBrush(color), x, y, 10, 10);
+
+                        x += 10;
+
+                        if (x >= 120)
+                        {
+                            x = 0;
+                            y += 10;
+                        }
+                    }
+                }
+
+                using (Graphics g = Graphics.FromImage(maxImage))
+                {
+                    int x = 0;
+                    int y = 0;
+
+                    foreach (PictureBox pb in panel1.Controls)
+                    {
+                        Color color = pb.BackColor;
+
+                        g.FillRectangle(new SolidBrush(color), x, y, 40, 40);
+
+                        x += 40;
+
+                        if (x >= 480)
+                        {
+                            x = 0;
+                            y += 40;
+                        }
+                    }
+                }
+
+                PictureBox miniPictureBox = new PictureBox
+                {
+                    Size = new Size(120, 120),
+                    Image = miniImage,
+                };
+
+                miniPictureBox.Click += (s, ev) => ShowImageInNewWindow(maxImage);
+
+                miniPictureBox.Location = new Point(currentX, currentY);
+
+                currentX += miniPictureBox.Width + padding;
+
+                if (currentX + miniPictureBox.Width > panel2.Width)
+                {
+                    currentX = padding;
+                    currentY += miniPictureBox.Height + padding;
+                }
+
+                panel2.Controls.Add(miniPictureBox);
+
+                await Task.Delay(500);
+                if (working == false)
+                {
+                    break;
+                }
+            }
+            button1.Enabled = true;
+            working = false;
+        }
+
+        private void ShowImageInNewWindow(Bitmap image)
+        {
+            Form newForm = new Form
+            {
+                Text = "Picture",
+                Size = new Size(480, 480)
+            };
+
+            PictureBox pictureBox = new PictureBox
+            {
+                SizeMode = PictureBoxSizeMode.Zoom,
+                Dock = DockStyle.Fill,
+                Image = image
+            };
+
+            Button btnSave = new Button
+            {
+                Text = "Save",
+                Size = new Size(75, 23),
+                Dock = DockStyle.Bottom,
+                DialogResult = DialogResult.OK
+            };
+            btnSave.Click += (sender, args) => SaveImage(image);
+
+            newForm.Controls.Add(pictureBox);
+            newForm.Controls.Add(btnSave);
+
+            newForm.ShowDialog();
+        }
+
+        private void SaveImage(Bitmap image)
+        {
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "Image PNG (*.png)|*.png|Image JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|All files (*.*)|*.*";
+                saveDialog.FilterIndex = 1;
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    System.Drawing.Imaging.ImageFormat imageFormat;
+
+                    switch (saveDialog.FilterIndex)
+                    {
+                        case 1:
+                            imageFormat = System.Drawing.Imaging.ImageFormat.Png;
+                            break;
+                        case 2:
+                            imageFormat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();//
+                    }
+
+                    image.Save(saveDialog.FileName, imageFormat);
+                    MessageBox.Show("Saved", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (working == true)
+            {
+                button2.Enabled = false;
+                working = false;
+                button2.Enabled = true;
+            }
+        }
+    }
+}
